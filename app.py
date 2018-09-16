be04e2e61e5df592780d0e1f1662cd012ff74b83
AlexGomesDS <Alexsantogomes@gmail.com>
2018-09-16 20:07:17 +0100

    [INI]-Added utils.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from peewee import (
     FloatField, TextField, IntegrityError
 )
 from playhouse.shortcuts import model_to_dict
-
+from utils import DataCleaner
 
 ########################################
 # Begin database stuff
